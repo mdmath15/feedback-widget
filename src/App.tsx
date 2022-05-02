980bb9b30d6388024213e931e1eb4bcfb261f21b
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2022-05-02 17:49:35 -0300

    componente widget e acessibilidade

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Widget } from "./components/Widget"
+
+export function App() {
+    return <>
+    <h1 className="text-5xl text-brand-500 ml-2">Feedback Widget</h1>
+    <Widget />
+    </>
+}
